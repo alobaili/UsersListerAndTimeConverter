848e4ff1e307b848a972e46c67c9b171fb03b2ae
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2021-11-10 22:01:55 +0300

    Initial Commit

---
+++ UsersListerAndTimeConverter/UsersListerAndTimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListerAndTimeConverterApp.swift
+//  UsersListerAndTimeConverter
+//
+//  Created by Abdulaziz AlObaili on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct UsersListerAndTimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
